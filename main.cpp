843393bcb69eed167828628b692faaaaf808dc67
A00835462 <A00835462@tec.mx>
2023-11-10 18:35:50 -0600

    .

--- main.cpp
+++ main.cpp
@@ -5,22 +5,48 @@
 // Evidencia 2 / Actividad Integradora 2
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 // MST
+/*
+Deberá el programa debe desplegar cuál es la forma óptima de cablear con una nueva fibra óptica conectando colonias de tal forma que se pueda compartir información entre cuales quiera dos colonias en el menor costo pósible, aprovechando que ya existen conexiones del nuevo cableado (las cuales no deben incluir en el costo)
+*/
 
 // TSP Kurskal
+/*
+Debido a que las ciudades apenas están entrando al mundo tecnológico, se requiere que alguien visite cada colonia que "no son centrales" para ir a dejar estados de cuenta físicos, publicidad, avisos y notificaciones impresos. por eso se quiere saber ¿cuál es la ruta más corta posible que visita cada colonia exactamente una vez y al finalizar regresa a la colonia origen? Tomar en cuenta que muchas veces el costo mínimo puede pasar por una colonia central o más intermedias. El programa debe desplegar la ruta a considerar así como el costo.
+*/
 
 // Floyd-Warshall
+/*
+El programa deberá generar la ruta óptima para ir de todas las centrales entre si, se puede pasar por una colonia no central.
+*/
+
 
 //
+/*
+Se leera a continuación una serie de puntos cartecianos en el mapa de la ciudad en donde se planea conectar nuevas colonias, y se deberá decir cual es la colina y punto carteciano más cercano con el cual se debe conectar.
+*/
 
 
 
 
 
 int main(){
+    int n, m, k, q;
+    //n = cantidad de colonias, 
+    //m = número de conexiones entre colonias, 
+    //k = las conexiones con el nuevo cableado, 
+    //q = futuras nuevas colonias que se desean conectar.
+    cin >> n >> m >> k >> q;
+    
+    string nCol; // Nombre de colonia
+    /*
+    
+    */
+
 
     return 0;
 }
